9e070c534d905b14ad09f5d7809b7b2559936e4b
Hck-king <72938430+Hck-king@users.noreply.github.com>
2025-07-09 22:45:00 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "unsafe-secret-key-for-dev-only
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1","portfoliopriyanshu.onrender.com").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1",).split(",")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
